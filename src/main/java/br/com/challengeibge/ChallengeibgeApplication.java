bd42e7d5fcce3b0ed2d495ba68372926caf069bf
Pedrosouza98 <pedro.henriquesouza98@gmail.com>
2021-04-14 22:24:21 -0300

    primeiro commit

---
+++ src/main/java/br/com/challengeibge/ChallengeibgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengeibge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeibgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeibgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challengeibge/ChallengeibgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challengeibge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeibgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
